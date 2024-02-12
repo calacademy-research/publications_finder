de22803860b97cd161699dfeb6212fbaf45bb3bc
Joe Russack <jrussack@calacademy.org>
2024-02-12 15:32:42 -0800

    initial

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from crossref import Crossref
 from db_connection import DBConnection
-
+from openalex_api import OpenAlex()
 
 #old and busted
 # def main():
@@ -13,6 +13,13 @@ from db_connection import DBConnection
 
 def main():
     from db_connection import DBConnection
+    # use some kind of flag - either a config option or a command line argument to determine
+    # whether we're going to re-pull the instutional level query to populate the database again.
+    open_alex =OpenAlex()
+    if (magical whatgever makese sense params go here):
+        open_alex.populate_instutition()
+    if (query_by_research_assocaites):
+        open_alex.query_by_author()
 
 
 if __name__ == "__main__":
--- openalex_api.py
+++ openalex_api.py
@@ -1,6 +1,7 @@
 from db_connection import DBConnection
 from config import Config
 class OpenAlex:
+    # we will keep this regardless
     def __init__(self):
         self.config = Config()
         # Joe - create the table here.
@@ -10,4 +11,17 @@ class OpenAlex:
                                     );"""
         DBConnection.execute_query(sql_create_database_table)
         self.ROR = self.config.get_string("alex_param", "ROR")
+
+    def populate_instutition(self):
+        # this is somehing maybe we call out to a third party API? maybe we use your
+        # existing code to query and do the pagination and all that good stuff
+        pass
+
+    def query_by_author(self):
+        pass
+
+    def insert_doi(self,array):
+        sql = f"insert bla bla into yadda {variable}"
+        DBConnection.execute_query(sql)
+
 
